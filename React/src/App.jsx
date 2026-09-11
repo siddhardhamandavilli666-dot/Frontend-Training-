7d09604cf3ec27557bd86becc8f4b47aefea8519
siddh <siddh@example.com>
2026-09-11 14:06:54 +0530

    updated

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -76,50 +76,139 @@
 
 // export default App
 
-import React,{useState,useEffect} from 'react'
+// import React,{useState,useEffect} from 'react'
+
+// const App = () => {
+//   const [users,setUsers]=useState([]);
+//   const fetchUsers=()=>
+//   {
+//     fetch("https://jsonplaceholder.typicode.com/users")
+//     .then(response=>response.json())
+//     .then(data=>setUsers(data))
+//   }
+//   //fetchUsers();
+//   useEffect(()=>{
+//     fetchUsers();
+//   },[])
+//   useEffect(()=>{
+//   console.log(users);
+//   },[users])
+//   return (
+//     <div>
+//      <h1>Users Data</h1>
+//      <table border="1">
+//       <thead>
+//       <tr>
+//         <td>id</td>
+//         <td>name</td>
+//         <td>username</td>
+//         <td>email</td>
+//       </tr>
+//       </thead>
+//       <tbody>
+//        {
+//         users.map((user)=>(
+//           <tr key={user.id} >
+//             <td>{user.id}</td>
+//             <td>{user.name}</td>
+//             <td>{user.username}</td>
+//             <td>{user.email}</td>
+//           </tr>
+//         ))
+//        }
+//       </tbody>
+//      </table>
+//     </div>
+//   )
+// }
+
+// export default App;
+
+// import React,{useState} from 'react'
+
+// const App = () => {
+//   const [text,setText]=useState("");
+//   return (
+//     <div>
+//       <input type="text" placeholder='Enter the name...' onChange={(e)=>setText(e.target.value)} />
+//       <p>{text}</p>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+// import React,{useState} from 'react'
+
+// const App = () => {
+//   const [name,setName]=useState("");
+//   const [submitted,setSubmitted]=useState("");
+//   function click(e)
+//   {
+//     e.preventDefault();
+//     setSubmitted(name);
+//   }
+//   return (
+//     <div>
+//       <form onSubmit={click}>
+//            <input type="text" placeholder='Enter the name:' onChange={(e)=>setName(e.target.value)}/>
+//            <button type="submit">Submit</button>
+//       </form>
+//       <h1>{submitted}</h1>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+import React,{useState} from 'react'
 
 const App = () => {
-  const [users,setUsers]=useState([]);
-  const fetchUsers=()=>
+  const [formData,setFormdata]=useState({name:"",email:"",pass:"",});
+  const [subForm,setSubForm]=useState(null);
+  function change(e)
   {
-    fetch("https://jsonplaceholder.typicode.com/users")
-    .then(response=>response.json())
-    .then(data=>setUsers(data))
+      setFormdata
+      (
+        {
+          ...formData,
+           [e.target.name]:e.target.value,
+
+        }
+      );
+
   }
-  //fetchUsers();
-  useEffect(()=>{
-    fetchUsers();
-  },[])
-  useEffect(()=>{
-  console.log(users);
-  },[users])
+  function click(e)
+  {
+       e.preventDefault();
+       setSubForm(formData);
+       setFormdata(
+           {name:"",email:"",pass:"",}
+       )
+  }
   return (
     <div>
-     <h1>Users Data</h1>
-     <table border="1">
-      <thead>
-      <tr>
-        <td>id</td>
-        <td>name</td>
-        <td>username</td>
-        <td>email</td>
-      </tr>
-      </thead>
-      <tbody>
+      <form onSubmit={click}>
+        <input type="text" placeholder='Enter the name:' onChange={change} name="name" value={formData.name}required />
+      <input type="email" placeholder='Enter the email:' onChange={change} name="email" value={formData.email} required/>
+      <input type="password" placeholder='Enter the password' onChange={change} name="pass"  value={formData.pass}required/>
+      <button type="submit">Submit</button>
+      </form>
        {
-        users.map((user)=>(
-          <tr key={user.id} >
-            <td>{user.id}</td>
-            <td>{user.name}</td>
-            <td>{user.username}</td>
-            <td>{user.email}</td>
-          </tr>
-        ))
+        subForm &&(
+          <div>
+            <h2>Form Submitted</h2>
+            <p>Name:{subForm.name}</p>
+            <p>Email:{subForm.email}</p>
+            <p>Password:{subForm.pass}</p>
+          </div>
+        )
        }
-      </tbody>
-     </table>
     </div>
   )
 }
 
 export default App
+
